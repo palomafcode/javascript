828569d2fa1623ffdb6b111e7ab6d94f46adabac
palomafcode <palomavillasboas380@gmail.com>
2022-10-31 21:19:50 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//comentario
+const nomeUsuario = document.querySelector('header .avatar-content div');
+nomeUsuario.innerHTML = '<strong>Nome Usuário</strong>';
+
+const primeiroNomeInput = document.getElementsByName('name')[0]
+primeiroNomeInput.placeholder = 'Digite o Primeiro Nome';
